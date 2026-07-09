a0f07f61109552c966a034acaa7d92058a07353e
Hoang An <hoangan726@gmail.com>
2026-07-09 00:07:39 -0700

    Bla bla

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/pofolio/',
+    base: '/Pofolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
